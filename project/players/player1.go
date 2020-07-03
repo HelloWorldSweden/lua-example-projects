9d7cc34aee8c0958ba3fe8a5b06e95c155f96210
Benjamin L <benjamin.lindsten@gmail.com>
2020-07-03 10:15:09 +0200

    Removed some code and objects

--- project/players/player1.go
+++ project/players/player1.go
@@ -1,18 +1,3 @@
-components {
-  id: "player1"
-  component: "/project/players/player1.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
